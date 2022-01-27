851e7c52b8ad00128ef4d2480e359607797b47e4
fatihyanik <fatihyanik07@gmail.com>
2022-01-27 15:48:02 +0100

    I added mailer

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require("express");
 require("dotenv").config()
 const path = require("path");
 
+// import emailSender
+const emailSender = require("./models/emailSender");
+
 const app = express();
 // set the port
 const port = process.env.PORT || 3000;
+
+// add middleware to get the data using POST request 
+app.use(express.urlencoded({extended:false})); // false it wil queryString parser
+app.use(express.json());
+
 // set public folder
 app.use(express.static(path.join(__dirname, "public")));
 // set ejs as view engine
@@ -36,6 +44,24 @@ app.get("/videos", (req, res) => {
   res.render("videos");
 });
 
+//callback solution
+/* app.post('/contact', (req, res)=>{
+  console.log(req.body);
+  emailSender.sendEmail(req.body, (data) => {
+    res.json(data)
+  })
+}) */
+
+//promise solution
+app.post('/contact', (req, res) => {
+  console.log(req.body);
+  emailSender.sendEmail(req.body).then((info) => {
+    console.log(info);
+      res.json({result: 'done'})
+  }).catch(error => {
+      res.json({result: 'error'})
+  })
+})
 // listener
 app.listen(port, () => {
   console.log(`node application is listening to port : ${port}`);
